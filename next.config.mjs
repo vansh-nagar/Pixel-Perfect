cced8d246eb0c0ed1a40c2bdfc9965d8c71b1cbf
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2025-12-29 08:06:52 +0530

    feat: Add motion component to Page and enhance social proof with additional Tweet ID

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 // next.config.mjs
 
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   devIndicators: false,
--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,13 +1,25 @@
-import MouseFollower2 from "@/components/pixel-perfect/mouse-follower2";
+"use client";
 import { LightDarkMode } from "@/components/ui/light-dark-mode";
+import { motion } from "motion/react";
 
 const Page = () => {
   return (
-    <div className=" min-h-screen">
+    <div className=" min-h-screen flex flex-col justify-center items-center">
       <div className=" fixed top-4 right-4">
         <LightDarkMode />
       </div>
-      <MouseFollower2 />
+
+      <motion.div
+        initial={{ scale: 0.5, y: 30, opacity: 0, filter: "blur(10px)" }}
+        whileInView={{ scale: 1, y: 0, opacity: 1, filter: "blur(0px)" }}
+        transition={{
+          duration: 0.3,
+          ease: "easeOut",
+        }}
+        viewport={{ once: true }}
+      >
+        hello
+      </motion.div>
     </div>
   );
 };

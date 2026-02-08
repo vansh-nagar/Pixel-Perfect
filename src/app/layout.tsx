d0e9541a8b07dc44e418a5a1038a3ef898e6fca7
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2026-02-08 15:24:48 +0530

    feat: integrate `Analytics` component into the layout for enhanced tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
+        <Analytics/>
       </body>
         </LenisProvider>
     </html>

dd0d1f04bc1b1934218e691854e7932747c008cb
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2026-03-10 21:51:46 +0530

    refactor: comment out FolderAnimation import in playground page

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,11 +1,9 @@
-import FolderAnimation from "do-not-share/folder";
 import React from "react";
-import ImageHoverAnimation from "registry/new-york/motion-framer/image-hover-animation";
 
 const Page = () => {
   return (
     <div className="h-screen w-full flex justify-center items-center">
-      <FolderAnimation />
+      {/* <FolderAnimation /> */}
     </div>
   );
 };

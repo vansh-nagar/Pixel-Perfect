361052213fe4c7bc63933da1cc84482d5d8f0bc5
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2026-04-10 00:49:31 +0530

    feat: add BottomMenu and SmoothDropdown components, update dependencies in package.json and package-lock.json

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,11 +1,65 @@
 "use client";
 
-import Satallite from "do-not-share/satallite";
-import SvgExploration from "do-not-share/svg-exploration";
-import { motion } from "framer-motion";
+import BottomMenu from "@/components/ui/bottom-menu";
+import SmallSmoothDropdown from "do-not-share/small-smooth-dropdown";
+import { useState } from "react";
+import useMeasure from "react-use-measure";
+import { motion } from "motion/react";
 
 const Page = () => {
-  return <div></div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [contentRef, contentBounds] = useMeasure();
+  const [content2Ref, content2Bounds] = useMeasure();
+  const openHeight = Math.max(
+    40,
+    Math.ceil(contentBounds.height + content2Bounds.height),
+  );
+
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen w-full">
+      {/* <SmallSmoothDropdown /> */}
+      <div className="flex justify-end items-start w-md h-[400px] ">
+        <motion.div
+          transition={{
+            type: "spring",
+            damping: 34,
+            stiffness: 380,
+            mass: 0.8,
+          }}
+          onClick={() => setIsOpen(!isOpen)}
+          animate={{
+            width: isOpen ? 220 : "auto",
+            height: isOpen ? openHeight : 40,
+          }}
+          className="border overflow-hidden relative cursor-pointer"
+        >
+          <motion.div
+            ref={content2Ref}
+            className="=flex items-center justify-center p-2"
+            style={{ pointerEvents: isOpen ? "none" : "auto" }}
+          >
+            {isOpen ? "Open" : "Close"}
+          </motion.div>
+
+          <div ref={contentRef}>
+            <motion.div
+              animate={{ opacity: isOpen ? 1 : 0 }}
+              className="flex flex-col p-2"
+              style={{ pointerEvents: isOpen ? "auto" : "none" }}
+            >
+              <div>Profile</div>
+              <div>Settings</div>
+              <div>Billing</div>
+              <div className="mt-2 text-destructive flex justify-end">
+                Logout
+              </div>
+            </motion.div>
+          </div>
+        </motion.div>{" "}
+      </div>
+      {/* <BottomMenu /> */}
+    </div>
+  );
 };
 
 export default Page;
